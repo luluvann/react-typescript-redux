9941353abb28d71cb043af9dce7c8cae3a94fe55
lucile <developer.lvt@gmail.com>
2023-02-24 15:12:12 -0500

    more recovered code

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThunkDispatch } from 'redux-thunk';
 import './App.css';
 import MoviesListComponent from './components/movies/MoviesListComponent';
 import PaginatorComponent from './design-system-components/paginator/PaginatorComponent';
+import Movie from './interfaces/Movie';
 import { changePage, displayMovies, fetchMovies } from './store/moviesActions';
 import { MoviesState } from './store/moviesReducers';
 import { MoviesActionTypes } from './store/moviesTypes';
@@ -14,6 +15,7 @@ function App() {
   const movies = useSelector((state: RootState) => state.movies.data);
   const numberOfPages = useSelector((state: RootState) => state.movies.numberOfPages);
   const currentPage = useSelector((state: RootState) => state.movies.currentPage);
+  const numberOfItemsPerPage = useSelector((state: RootState) => state.movies.numberOfItemsPerPage);
 
   function handlePageChange() {
     dispatch(changePage(1))
@@ -23,8 +25,21 @@ function App() {
     dispatch(displayMovies(4))
     console.log("change")
   }
-  
+
+  function groupArrayByN(array: Movie[], n: number): Movie[][] {
+    const groups: Movie[][] = [];
+    let i: number = 0;
+
+    while (i < array.length) {
+      groups.push(array.slice(i, i + n));
+      i += n;
+    }
+
+    return groups;
+  }
+
   const filteredMovies = movies.filter((movie) => movie.isShowing == true)
+  const groupedMovies = groupArrayByN(movies, numberOfItemsPerPage)
 
   useEffect(() => {
     dispatch(fetchMovies());
@@ -33,8 +48,11 @@ function App() {
   return (
     <div className="App">
       {/* <DropdownComponent movies={movies} /> */}
-      <MoviesListComponent index={1} movies={filteredMovies} />
-      <PaginatorComponent numberOfPages={numberOfPages} currentPage={currentPage} displayOptions={[4,8,20]} handleOptionChange={handleOptionChange} handlePageChange={handlePageChange}/>
+      {groupedMovies.map((group, index) => (
+        <MoviesListComponent index={index + 1} movies={group} />
+      ))}
+
+      <PaginatorComponent numberOfPages={numberOfPages} currentPage={currentPage} displayOptions={[4, 8, 20]} handleOptionChange={handleOptionChange} handlePageChange={handlePageChange} />
     </div>
   );
 }
