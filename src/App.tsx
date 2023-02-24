cd455c18c309ba3623fee0bb18bad66a4e9b50c5
lucile <developer.lvt@gmail.com>
2023-02-24 18:01:31 -0500

    cleaned App file

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,37 @@ import { MoviesActionTypes } from './store/moviesTypes';
 import { RootState } from './store/store';
 
 function App() {
+
+  // Get store data
   const dispatch = useDispatch<ThunkDispatch<MoviesState, undefined, MoviesActionTypes>>();
   const movies = useSelector((state: RootState) => state.movies.data);
   const currentPage = useSelector((state: RootState) => state.movies.currentPage);
   const numberOfItemsPerPage = useSelector((state: RootState) => state.movies.numberOfItemsPerPage);
 
+  // Fetch the movies data
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+
+  // Handlers and dispatch
   function handleOptionChangeMultiselect(event: React.ChangeEvent<HTMLSelectElement>) {
     const selectedOptions = Array.from(event.target.options)
       .filter(option => option.selected)
       .map(option => option.value);
-      console.log(selectedOptions)
-    dispatch(filterMovies(selectedOptions))
+    dispatch(filterMovies(selectedOptions));
   }
 
   function handlePageChange(pageClicked: number) {
-    dispatch(changePage(pageClicked))
+    dispatch(changePage(pageClicked));
   }
 
   function handleOptionChangePaginator(event: React.ChangeEvent<HTMLSelectElement>) {
-    dispatch(displayMovies(parseInt(event.target.value)))
+    dispatch(displayMovies(parseInt(event.target.value)));
 
   }
 
+  // Grouping handler (for pages management)
   function groupArrayByN(array: Movie[], n: number): Movie[][] {
     const groups: Movie[][] = [];
     let i: number = 0;
@@ -48,6 +57,7 @@ function App() {
   const filteredMovies = movies.filter((movie) => movie.isShowing == true)
   const groupedMovies = groupArrayByN(filteredMovies, numberOfItemsPerPage)
 
+  // Components properties objects builders
   const paginatorProperties: PaginatorProps = {
     totalNumberOfItems: filteredMovies.length,
     numberOfItemsToDisplayPerPage: numberOfItemsPerPage,
@@ -64,10 +74,6 @@ function App() {
     })
     return categoryOptions
   }
-
-  useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
 
   return (
     <div className="App">
